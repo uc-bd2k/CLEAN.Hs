e16df0782ad1e97206a13321749ec8b1f282e05d
medvedm <medvedm@ucmail.uc.edu>
2019-11-23 08:39:59 -0500

    adding dorotheatfs

--- R/CLEAN.Hs.R
+++ R/CLEAN.Hs.R
@@ -1,3 +1,3 @@
 CLEAN.Hs <-
-function() c("BioGRID", "chrLoc", "CpGislands", "ESR1targets", "geneRIFs", "GO", "GSEA", "HUGE", "KEGG", "L2L", "MeshTable", "methylome", "modules", "MP", "reactome", "transfac","CTD","tfacts","chiptfs","tissues","rppa","mtor")
+function() c("BioGRID", "chrLoc", "CpGislands", "ESR1targets", "geneRIFs", "GO", "GSEA", "HUGE", "KEGG", "L2L", "MeshTable", "methylome", "modules", "MP", "reactome", "transfac","CTD","tfacts","chiptfs","tissues","rppa","mtor", "dorotheatfs")
 
